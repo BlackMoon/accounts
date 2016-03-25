b4b93f9585a49e3323cf9af3d922d38ddb180c42
Роман Романов <romanovrv@tatintec.ru>
2016-03-25 13:53:22 +0300

    Accounts

---
+++ Modules/AuthModule.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices;
+using Nancy;
+using Nancy.Security;
+
+namespace Accounts.Modules
+{
+    public class AccountModule : NancyModule
+    {
+        public AccountModule()
+        {
+            Get[""] = Get["/"] = Get["/login"] = parameters =>
+            {
+                var model = new {title = "Hello, world!"};
+                return View["login", model];
+            };
+
+            Get["/logout"] = _ => View["home"];
+
+            Post["/login"] = parameters => {
+                
+                return null;
+            };
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using Nancy.Owin;
+
+namespace Accounts
+{
+    public class Startup
+    {
+        internal static IApplicationEnvironment AppEnvironment { get; private set; }
+
+        public IConfigurationRoot Configuration { get; set; }
+
+        public Startup(IHostingEnvironment hostEnv, IApplicationEnvironment appEnv)
+        {
+            AppEnvironment = appEnv;
+            
+            // Set up configuration sources.
+
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{hostEnv.EnvironmentName}.json", true);
+
+            if (hostEnv.IsDevelopment())
+            {
+                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+                //builder.AddApplicationInsightsSettings(true);
+
+            }
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseIISPlatformHandler();
+
+            app.UseOwin(a => a.UseNancy());
+            
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
